ac25166d4f51502a81b888c2c807b129667007ab
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-06-20 05:23:04 +0900

    Add AppConfig struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod cli;
+pub mod config;
 pub mod error;
 pub mod profile;
 pub mod transfer;
 
 pub use cli::*;
+pub use config::*;
 pub use error::*;
 pub use profile::*;
 pub use transfer::*;
